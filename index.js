3e87623c2f7c129a55cac04a599318606f7bdf3b
madhan mohan <madhan.murugesan@onegservice.com>
2017-09-22 15:39:37 +0530

    deploying in heroku

---
+++ index.js
@@ -0,0 +1,9 @@
+const express= require ('express');
+const app= express();
+app.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ name: product1, price:1.00, vendorid:vendor1 }));
+})
+app.listen(process.env.PORT || 8000,()=>{
+    console.log("app is running");
+})
