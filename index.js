d423f05a9c1aad4638f688e072506ae711c96f4f
madhan mohan <madhan.murugesan@onegservice.com>
2017-09-25 16:59:39 +0530

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express= require ('express');
 const app= express();
-app.get('/', function (req, res) {
+const bodyParser =require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+app.get('/scanProduct', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ name: "product1", price:"1.00", vendorid:"vendor1" }));
+})
+app.post('/productAdd', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ name: "product1", price:"1.00", vendorid:"vendor1" }));
 })
